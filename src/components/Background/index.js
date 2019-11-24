5c7024bef5bf1f3ca6142a4b36e37c1ac5fd9a90
aronedwards91 <aronedwards91@users.noreply.github.com>
2019-11-24 12:05:01 +0000

    Initial commit

---
+++ src/components/Background/index.js
@@ -0,0 +1,31 @@
+import { h } from "preact";
+import style from "./style.css";
+
+const Background = () => (
+  <div>
+    <div class="background">
+      <div class={style.backgroundWater}></div>
+    </div>
+    <svg>
+      <filter id="turbulence" x="0" y="0" width="100%" height="100%">
+        <feTurbulence
+          id="sea-filter"
+          numOctaves="3"
+          seed="2"
+          baseFrequency="0.02 0.05"
+        ></feTurbulence>
+        <feDisplacementMap scale="20" in="SourceGraphic"></feDisplacementMap>
+        <animate
+          xlinkHref="#sea-filter"
+          attributeName="baseFrequency"
+          dur="60s"
+          keyTimes="0;0.5;1"
+          values="0.02 0.06;0.04 0.08;0.02 0.06"
+          repeatCount="indefinite"
+        />
+      </filter>
+    </svg>
+  </div>
+);
+
+export default Background;
