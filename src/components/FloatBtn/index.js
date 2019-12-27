c8563e8f94462677a5a80f3af41a6a07dc23c993
herotica <aronedwards91@users.noreply.github.com>
2019-12-27 09:58:59 +0000

    setup routing

--- src/components/FloatBtn/index.js
+++ src/components/FloatBtn/index.js
@@ -1,13 +1,17 @@
 import { h } from "preact";
-// import style from "./style.css";
+import { route } from 'preact-router';
+import routes from "../../utils/routing.json";
 import style from './style';
 import { useState } from "preact/hooks";
 
-const FloatBtn = ({text}) => {
+const FloatBtn = ({text, path}) => {
   const [clicked, setClicked] = useState(false);
-  console.log("cl:", clicked);
+  const handleClick = () => {
+    route(path);
+    setClicked(!clicked)
+  }
   return (
-    <div onClick={() => setClicked(!clicked)}>
+    <div onClick={handleClick}>
       <div class={style.button}>
         <div class={style.button_text}>{text}</div>
       </div>
@@ -16,22 +20,22 @@ const FloatBtn = ({text}) => {
 };
 
 export const WelcomeBtn = () => (
-  <FloatBtn text="Welcome" />
-);
-export const SkillsBtn = () => (
-  <FloatBtn text="Skills" />
+  <FloatBtn text="Welcome" path={routes.welcome} />
 );
 export const ExperienceBtn = () => (
-  <FloatBtn text="Experience" />
-);
-export const DemoBtn = () => (
-  <FloatBtn text="Demo" />
+  <FloatBtn text="Experience" path={routes.experience}/>
 );
 export const BookListBtn = () => (
-  <FloatBtn text="Book List" />
+  <FloatBtn text="Book List" path={routes.booklist} />
+);
+export const SkillsBtn = () => (
+  <FloatBtn text="Skills" path={routes.skills} />
+);
+export const DemoBtn = () => (
+  <FloatBtn text="Demo" path={routes.demo} />
 );
 export const ContactBtn = () => (
-  <FloatBtn text="Contact" />
+  <FloatBtn text="Contact" path={routes.contact} />
 );
 
 export default FloatBtn;
