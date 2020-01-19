2a1f3f9586de75548d855c17f34665af9638d4c4
herotica <aronedwards91@users.noreply.github.com>
2020-01-19 19:17:50 +0000

    Mobile bg added

--- src/components/Background/index.js
+++ src/components/Background/index.js
@@ -1,5 +1,6 @@
 import { h } from "preact";
 import style from "./style";
+import { MobileShow, MobileHide } from "../Mobile";
 
 const Background = () => {
   let local = false;
@@ -12,33 +13,38 @@ const Background = () => {
 
   return (
     <div class={style.bgContainer}>
-      <div class={style.background}>
-        <div class={style.backgroundWater}></div>
-      </div>
-      {!local ? (
-        <svg class={style.svgHide}>
-          <filter id="turbulence" x="0" y="0" width="100%" height="100%">
-            <feTurbulence
-              id="sea-filter"
-              numOctaves="3"
-              seed="2"
-              baseFrequency="0.02 0.05"
-            ></feTurbulence>
-            <feDisplacementMap
-              scale="20"
-              in="SourceGraphic"
-            ></feDisplacementMap>
-            <animate
-              xlinkHref="#sea-filter"
-              attributeName="baseFrequency"
-              dur="60s"
-              keyTimes="0;0.3;0.4;0.6;0.8;1"
-              values="0.02 0.06;0.03 0.07;0.04 0.08;0.035 0.075;0.03 0.07;0.02 0.06"
-              repeatCount="indefinite"
-            />
-          </filter>
-        </svg>
-      ) : null}
+      <MobileShow>
+        <div class={style.backgroundSVG} />
+      </MobileShow>
+      <MobileHide>
+        <div class={style.background}>
+          <div class={style.backgroundWater}></div>
+        </div>
+        {!local ? (
+          <svg class={style.svgHide}>
+            <filter id="turbulence" x="0" y="0" width="100%" height="100%">
+              <feTurbulence
+                id="sea-filter"
+                numOctaves="3"
+                seed="2"
+                baseFrequency="0.02 0.05"
+              ></feTurbulence>
+              <feDisplacementMap
+                scale="20"
+                in="SourceGraphic"
+              ></feDisplacementMap>
+              <animate
+                xlinkHref="#sea-filter"
+                attributeName="baseFrequency"
+                dur="60s"
+                keyTimes="0;0.3;0.4;0.6;0.8;1"
+                values="0.02 0.06;0.03 0.07;0.04 0.08;0.035 0.075;0.03 0.07;0.02 0.06"
+                repeatCount="indefinite"
+              />
+            </filter>
+          </svg>
+        ) : null}
+      </MobileHide>
     </div>
   );
 };
